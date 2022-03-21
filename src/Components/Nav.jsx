0aa32c57b1f053150a9b50b0bdf2b436b0ab14ee
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-03-21 09:37:21 -0400

    updating homepage

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav>
       <ul className="nav-links">
-        <NavLink style={{ textDecoration: "none" }} to="/"> <h1>Home</h1></NavLink>
+        <NavLink style={{ textDecoration: "none" }} to="/Monster-Management-Front-End"> <h1>Home</h1></NavLink>
         <NavLink  style={{ textDecoration: "none", color: "white" }}to="/view-monster/:id"> 
         <h1>View monster table </h1>
         </NavLink>
